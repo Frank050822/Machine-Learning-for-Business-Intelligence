f3f7e291c9b3d8dcb303f552efca91bdd019b034
Frank050822 <56621708+Frank050822@users.noreply.github.com>
2020-03-03 14:24:23 -0500

    Create Table using SQL

---
+++ create.sql
@@ -0,0 +1,18 @@
+create table userinfo (
+	userid varchar(40) NOT NULL primary key,
+	username varchar(20) NOT NULL ,
+	gender set('male','female','secret') not null default 'secret',
+	birthdate DATE NOT NULL default '1949-01-01',
+	pwd varchar(20) not null,
+	question varchar(30) ,
+	answer varchar(30) ,
+	email varchar(50) ,
+	photopath varchar(50) ,
+	intro varchar(300) ,
+	role enum('0','1') not null default '0' ,
+	regtime timestamp not null DEFAULT CURRENT_TIMESTAMP() 
+);
+
+// Compare & Comtrast 
+insert into uerinfo(name, gender) values('Tom', 'male')
+"insert into userinfo(name, gender) values('".$name."', '".$gender."')"
